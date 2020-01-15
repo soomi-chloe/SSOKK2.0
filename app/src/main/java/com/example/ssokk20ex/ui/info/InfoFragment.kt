3b8a0c247fea425be4b9e8a67f3a83fb831ff35b
Jdev <109soomi@naver.com>
2020-01-15 15:54:04 +0900

    infoFragment 수정

--- app/src/main/java/com/example/ssokk20ex/ui/info/InfoFragment.kt
+++ app/src/main/java/com/example/ssokk20ex/ui/info/InfoFragment.kt
@@ -22,10 +22,6 @@ class InfoFragment : Fragment() {
         infoViewModel =
             ViewModelProviders.of(this).get(InfoViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_info, container, false)
-        val textView: TextView = root.findViewById(R.id.text_info)
-        infoViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
 }
