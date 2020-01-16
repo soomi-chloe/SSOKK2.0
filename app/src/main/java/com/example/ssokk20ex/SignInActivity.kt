ac7e391730cf7e4a121289f45186702ef25e85d7
shalo1040 <kimd02@pfw.edu>
2020-01-16 17:47:16 +0900

    sign in and sign up

--- app/src/main/java/com/example/ssokk20ex/SignInActivity.kt
+++ app/src/main/java/com/example/ssokk20ex/SignInActivity.kt
@@ -3,17 +3,44 @@ package com.example.ssokk20ex
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
+    var auth: FirebaseAuth? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        supportActionBar?.hide()
+
+        auth = FirebaseAuth.getInstance()
 
         signInBtn.setOnClickListener {
+            emailLogin()
+        }
+
+        createAccountBtn.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivityForResult(intent, 0)
         }
+    }
+
+    private fun emailLogin() {
+        if(signInTxt.text.toString().isNullOrEmpty() || pwTxt.text.toString().isNullOrEmpty()) {
+            Toast.makeText(this, "이메일과 비밀번호를 입력해주세요", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        auth?.signInWithEmailAndPassword(emailTxt.text.toString(), pwTxt.text.toString())
+            ?.addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this, "정보가 틀립니다. 다시 입력해주세요", Toast.LENGTH_LONG).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/ssokk20ex/SignUpActivity.kt
+++ app/src/main/java/com/example/ssokk20ex/SignUpActivity.kt
@@ -1,12 +1,48 @@
 package com.example.ssokk20ex
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
+    var auth: FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        supportActionBar?.hide()
+
+        auth = FirebaseAuth.getInstance()
+
+        signUpBtn.setOnClickListener {
+            createAccount()
+        }
+    }
+
+    private fun createAccount() {
+        if(emailTxt.text.toString().isNullOrEmpty() || pwTxt.text.toString().isNullOrEmpty() || pwCheckTxt.text.toString().isNullOrEmpty()) {
+            Toast.makeText(this, "모든 항목을 입력해주세요", Toast.LENGTH_LONG).show()
+            return
+        }
+        if(pwTxt.text.toString() == pwCheckTxt.text.toString()) {
+            auth?.createUserWithEmailAndPassword(emailTxt.text.toString().trim(), pwTxt.text.toString().trim())
+                ?.addOnCompleteListener { task ->
+                    if(task.isSuccessful) {
+                        val intent = Intent(this, SignInActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+        } else {
+            Toast.makeText(this, "비밀번호가 틀립니다", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
