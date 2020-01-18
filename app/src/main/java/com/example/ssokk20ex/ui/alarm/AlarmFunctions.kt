73e4065d70135e5d784061ab32f0f293a4f6e6da
Sooooooom <109soomi@naver.com>
2020-01-18 19:10:09 +0900

    alarm modified

--- app/src/main/java/com/example/ssokk20ex/ui/alarm/AlarmFunctions.kt
+++ app/src/main/java/com/example/ssokk20ex/ui/alarm/AlarmFunctions.kt
@@ -1,8 +1,10 @@
 package com.example.ssokk20ex.ui.alarm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ssokk20ex.R
+import kotlinx.android.synthetic.main.activity_blood_sugar_notice.*
 
 class AlarmFunctions : AppCompatActivity() {
 
@@ -10,5 +12,13 @@ class AlarmFunctions : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blood_sugar_notice)
         supportActionBar?.hide()
+
+        tab_blood_sugar.setOnClickListener {
+            startActivity(Intent(this, AlarmFunctions::class.java))
+        }
+
+        tab_pill.setOnClickListener {
+            startActivity(Intent(this, MedicineNotice::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/ssokk20ex/ui/alarm/MedicineNotice.kt
+++ app/src/main/java/com/example/ssokk20ex/ui/alarm/MedicineNotice.kt
@@ -1,8 +1,10 @@
 package com.example.ssokk20ex.ui.alarm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ssokk20ex.R
+import kotlinx.android.synthetic.main.activity_blood_sugar_notice.*
 
 class MedicineNotice : AppCompatActivity() {
 
@@ -10,5 +12,13 @@ class MedicineNotice : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medicine_notice)
         supportActionBar?.hide()
+
+        tab_blood_sugar.setOnClickListener {
+            startActivity(Intent(this, AlarmFunctions::class.java))
+        }
+
+        tab_pill.setOnClickListener {
+            startActivity(Intent(this, MedicineNotice::class.java))
+        }
     }
 }
