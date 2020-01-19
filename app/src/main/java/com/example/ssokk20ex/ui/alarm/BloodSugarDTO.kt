87915c2119b16abe6979f3c95d3a303b103aa39d
shalo1040 <kimd02@pfw.edu>
2020-01-20 01:39:26 +0900

    AddBloodSugar 아마 완성?

--- app/src/main/java/com/example/ssokk20ex/ui/alarm/BloodSugarDTO.kt
+++ app/src/main/java/com/example/ssokk20ex/ui/alarm/BloodSugarDTO.kt
@@ -1,8 +1,30 @@
 package com.example.ssokk20ex.ui.alarm
 
-data class BloodSugarDTO(val alarmDate: String? = null,
-                         val alarmTotalN: String? = null,
-                         val alarm_1:String? = null,
-                         val alarm_1_MealBeforeOrAfter:String? = null,
-                         val alarm_1_Time:String? = null
+data class BloodSugarDTO(var alarmDate: String? = null,
+                         var alarmTotalN: String? = null,
+
+                         var alarm_1:String? = null,
+                         var alarm_1_MealBeforeOrAfter:String? = null,
+                         var alarm_1_Time:String? = null,
+
+                         var alarm_2:String? = null,
+                         var alarm_2_MealBeforeOrAfter:String? = null,
+                         var alarm_2_Time:String? = null,
+
+                         var alarm_3:String? = null,
+                         var alarm_3_MealBeforeOrAfter:String? = null,
+                         var alarm_3_Time:String? = null,
+
+                         var alarm_4:String? = null,
+                         var alarm_4_MealBeforeOrAfter:String? = null,
+                         var alarm_4_Time:String? = null,
+
+                         var alarm_5:String? = null,
+                         var alarm_5_MealBeforeOrAfter:String? = null,
+                         var alarm_5_Time:String? = null,
+
+                         var alarm_6:String? = null,
+                         var alarm_6_MealBeforeOrAfter:String? = null,
+                         var alarm_6_Time:String? = null
+
 )
